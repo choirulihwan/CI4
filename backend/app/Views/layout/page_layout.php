021617b0e72935dc841eefb6adc18c23843d60f2
choirul <aristhu_oracle@yahoo.com>
2022-10-26 14:01:36 +0700

    fixing label

--- backend/app/Views/layout/page_layout.php
+++ backend/app/Views/layout/page_layout.php
@@ -22,17 +22,17 @@
 
 	<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
 		<div class="container">
-			<a class="navbar-brand" href="<?= base_url() ?>">Home</a>
+			<a class="navbar-brand" href="<?= base_url() ?>">Beranda</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarNav">
 				<ul class="navbar-nav">
 					<li class="nav-item">
-						<a class="nav-link" href="<?=base_url('manquiz') ?>">Quiz</a>						
+						<a class="nav-link" href="<?=base_url('manquiz') ?>">Pertanyaan</a>						
 					</li>
 					<li class="nav-item">	
-						<a class="nav-link" href="<?=base_url('mancategory') ?>">Mata pelajaran</a>	
+						<a class="nav-link" href="<?=base_url('mancategory') ?>">Pelajaran</a>	
 					</li>
 					<li class="nav-item">	
 						<a class="nav-link" href="<?=base_url('users/logout') ?>">Sign out</a>	
